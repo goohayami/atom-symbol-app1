4d135976c9ec5d3e790063d0395fa0ecef8a0678
goohayami <98527002+goohayami@users.noreply.github.com>
2022-07-26 17:59:36 +0900

    Add files via upload

--- src/atomicSymbol.js
+++ src/atomicSymbol.js
@@ -11,6 +11,25 @@ for (let i = 0; i < atomicSymbol.symbol.length; i++) {
 
 for (let i = 0; i < atomicSymbol.name.length; i++) {
   const nameContainer = document.getElementById("name" + [i]);
-  console.log("name" + [i]);
   nameContainer.innerHTML = `${atomicSymbol.name[i]}`;
+}
+
+// 入力されたテキストを取得
+for (let i = 0; i < 2; i++) {
+  const inputBtn = document.getElementById("btn" + [i]);
+  const nameContainer = document.getElementById("name" + [i]);
+
+  inputBtn.addEventListener("click", () => {
+    const inputText = document.getElementById("input" + [i]).value;
+
+    console.log(inputText);
+    console.log(atomicSymbol.name[i]);
+
+    if (inputText === atomicSymbol.name[i]) {
+      alert("おめでとう");
+      nameContainer.classList.add("symbol-appear");
+    } else {
+      alert("もういちど");
+    }
+  });
 }
